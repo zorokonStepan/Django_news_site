c10634d972ac8ea812e44877dec39c180b5cf5b0
Stepan Kolodkin <zorokon@yandex.ru>
2022-01-13 14:34:22 +0300

    migration 0001

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,23 @@
+from tkinter import Image
+from xmlrpc.client import DateTime
+
 from django.db import models
 
+
 # Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    is_published = models.BooleanField(default=True)
+
+    # id - INT
+    # title - Varchar
+    # content - Text
+    # created_at - DateTime
+    # updated_at - DateTime
+    # photo - Image
+    # is_published - Boolean
