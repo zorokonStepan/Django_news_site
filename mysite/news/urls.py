71cba5dec97adf62efeba594795c7470105f52c3
Stepan Kolodkin <zorokon@yandex.ru>
2022-01-14 18:09:45 +0300

    pattern inheritance

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from news.views import index, get_category
 
 urlpatterns = [
-    path('', index),
-    path('category/<int:category_id>/', get_category),
+    path('', index, name="home"),
+    path('category/<int:category_id>/', get_category, name="category"),
 ]
