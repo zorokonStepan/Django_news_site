fc074a2448cd6f5d96a3e4eac713620b1ec1c4e7
Stepan Kolodkin <zorokon@yandex.ru>
2022-01-15 11:08:02 +0300

    make custom tags

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -5,14 +5,15 @@ from .models import News, Category
 
 def index(request: HttpRequest) -> HttpResponse:
     news = News.objects.all()
-    categories = Category.objects.all()
-    context = {"news": news, "title": "Список новостей", "categories": categories}
+    context = {"news": news, "title": "Список новостей"}
     return render(request=request, template_name='news/index.html', context=context)
 
 
 def get_category(request: HttpRequest, category_id: int) -> HttpResponse:
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    context = {"news": news, "categories": categories, "category": category}
+    context = {"news": news, "category": category}
     return render(request=request, template_name='news/category.html', context=context)
+
+
+
